aac01d30368771aa05dc47a911e55366ae2cb0b5
Soy Alejandro <pilodaniel4000@gmail.comgit config --listgit config --global user.email pilodaniel4000@gmail.com>
2026-05-22 14:34:47 -0500

    se realiza el commit de la carpeta routers con sus respectivos archivos

---
+++ login/routers/commentsRouter.go
@@ -0,0 +1,145 @@
+package routers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:AdministradorController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:LoginController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"],
+		beego.ControllerComments{
+			Method:           "GetOne",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"] = append(beego.GlobalControllerRouter["the_house_fit/controllers:UsuariosController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           `/:id`,
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+}
