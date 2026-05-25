2b4bfdfa970831c04820c55415529f39abf32fc2
Daniel Alejandro Suarez Rodriguez <pilodaniel4000@gmail.com>
2026-05-25 15:48:29 -0500

    se realiza las modificaciones de json

--- login/controllers/usuarios.go
+++ login/controllers/usuarios.go
@@ -2,11 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
-	"errors"
 	"strconv"
 	"strings"
 	"the_house_fit/models"
 
+	"github.com/beego/beego/v2/core/logs"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
@@ -25,57 +25,40 @@ func (c *UsuariosController) URLMapping() {
 }
 
 // Post ...
-// @Title Post
-// @Description create Usuarios
-// @Param	body		body 	models.Usuarios	true		"body for Usuarios content"
-// @Success 201 {int} models.Usuarios
-// @Failure 403 body is empty
 // @router / [post]
 func (c *UsuariosController) Post() {
 	var v models.Usuarios
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if _, err := models.AddUsuarios(&v); err == nil {
 			c.Ctx.Output.SetStatus(201)
-			c.Data["json"] = v
+			c.Data["json"] = map[string]interface{}{"success": true, "status": 201, "Message": "Usuario creado exitosamente", "data": v}
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error al crear el Usuario: " + err.Error(), "data": nil}
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error al leer el cuerpo de la solicitud: " + err.Error(), "data": nil}
 	}
 	c.ServeJSON()
 }
 
 // GetOne ...
-// @Title Get One
-// @Description get Usuarios by id
-// @Param	id		path 	string	true		"The key for staticblock"
-// @Success 200 {object} models.Usuarios
-// @Failure 403 :id is empty
 // @router /:id [get]
 func (c *UsuariosController) GetOne() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	v, err := models.GetUsuariosById(id)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error en el servicio GetOne: La solicitud contiene un parametro incorrecto o no existe ningun registro", "data": nil}
 	} else {
-		c.Data["json"] = v
+		c.Data["json"] = map[string]interface{}{"success": true, "status": 200, "Message": "Peticion exitosa", "data": v}
 	}
 	c.ServeJSON()
 }
 
 // GetAll ...
-// @Title Get All
-// @Description get Usuarios
-// @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
-// @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
-// @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
-// @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
-// @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
-// @Param	offset	query	string	false	"Start position of result set. Must be an integer"
-// @Success 200 {object} models.Usuarios
-// @Failure 403
 // @router / [get]
 func (c *UsuariosController) GetAll() {
 	var fields []string
@@ -85,32 +68,26 @@ func (c *UsuariosController) GetAll() {
 	var limit int64 = 10
 	var offset int64
 
-	// fields: col1,col2,entity.col3
 	if v := c.GetString("fields"); v != "" {
 		fields = strings.Split(v, ",")
 	}
-	// limit: 10 (default is 10)
 	if v, err := c.GetInt64("limit"); err == nil {
 		limit = v
 	}
-	// offset: 0 (default is 0)
 	if v, err := c.GetInt64("offset"); err == nil {
 		offset = v
 	}
-	// sortby: col1,col2
 	if v := c.GetString("sortby"); v != "" {
 		sortby = strings.Split(v, ",")
 	}
-	// order: desc,asc
 	if v := c.GetString("order"); v != "" {
 		order = strings.Split(v, ",")
 	}
-	// query: k:v,k:v
 	if v := c.GetString("query"); v != "" {
 		for _, cond := range strings.Split(v, ",") {
 			kv := strings.SplitN(cond, ":", 2)
 			if len(kv) != 2 {
-				c.Data["json"] = errors.New("Error: invalid query key/value pair")
+				c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error: par clave/valor de query invalido", "data": nil}
 				c.ServeJSON()
 				return
 			}
@@ -121,20 +98,15 @@ func (c *UsuariosController) GetAll() {
 
 	l, err := models.GetAllUsuarios(query, fields, sortby, order, offset, limit)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error en el servidor GetAll: la solicitud contiene un parametro incorrecto o no contiene registro solicitado", "data": nil}
 	} else {
-		c.Data["json"] = l
+		c.Data["json"] = map[string]interface{}{"success": true, "status": 200, "Message": "Peticion exitosa", "data": l}
 	}
 	c.ServeJSON()
 }
 
 // Put ...
-// @Title Put
-// @Description update the Usuarios
-// @Param	id		path 	string	true		"The id you want to update"
-// @Param	body		body 	models.Usuarios	true		"body for Usuarios content"
-// @Success 200 {object} models.Usuarios
-// @Failure 403 :id is not int
 // @router /:id [put]
 func (c *UsuariosController) Put() {
 	idStr := c.Ctx.Input.Param(":id")
@@ -142,30 +114,28 @@ func (c *UsuariosController) Put() {
 	v := models.Usuarios{Id: id}
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if err := models.UpdateUsuariosById(&v); err == nil {
-			c.Data["json"] = "OK"
+			c.Data["json"] = map[string]interface{}{"success": true, "status": 200, "Message": "Usuario actualizado exitosamente", "data": v}
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error al actualizar el Usuario: " + err.Error(), "data": nil}
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error al leer el cuerpo de la solicitud: " + err.Error(), "data": nil}
 	}
 	c.ServeJSON()
 }
 
 // Delete ...
-// @Title Delete
-// @Description delete the Usuarios
-// @Param	id		path 	string	true		"The id you want to delete"
-// @Success 200 {string} delete success!
-// @Failure 403 id is empty
 // @router /:id [delete]
 func (c *UsuariosController) Delete() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	if err := models.DeleteUsuarios(id); err == nil {
-		c.Data["json"] = "OK"
+		c.Data["json"] = map[string]interface{}{"success": true, "status": 200, "Message": "Usuario eliminado exitosamente", "data": nil}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		c.Data["json"] = map[string]interface{}{"success": false, "status": 400, "Message": "Error al eliminar el Usuario: " + err.Error(), "data": nil}
 	}
 	c.ServeJSON()
 }
